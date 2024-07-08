99ec01bfcde18ca35f1429addd5f3f11a297c0bc
nida <nidazeeshan15@gmail.com>
2024-07-08 19:22:43 +0500

    upd

--- crud.py
+++ crud.py
@@ -1,3 +1,6 @@
 from sqlalchemy.orm import Session
 from models import User
 from schemas import *
+
+def create_user(db: Session , user: UserCreate):
+    db_user = User(name = user.name, email = user.email)
--- schemas.py
+++ schemas.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
 
 class UserCreate(BaseModel):
+    name:str
+    email:str
     
