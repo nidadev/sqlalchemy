d3bb36fb36a9963f879b42fac55166ce22e6903f
nida <nidazeeshan15@gmail.com>
2024-07-08 19:16:28 +0500

    upd

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
-from database import Base,engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import *
+from schemas import UserCreate
 
 app = FastAPI()
 
 Base.metadata.create_all(bind = engine)
+
+def get_db():
+    db = SessionLocal()
+    try :
+        yield db
+    finally:
+        db.close
+
+@app.post("user/create")
+async def create_user_api(user:UserCreate,db = Session = Depends(get_db)):
